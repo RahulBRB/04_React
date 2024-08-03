aff6b37145d7ab43cda806d1bdbd16bd57d96e83
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-08-03 14:23:12 +0530

    Add files via upload

---
+++ 02_Components/component_in_render_func.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from 'NavBar.js';
+
+function ProfilePage() {
+  return (
+    <div>
+      <NavBar />
+      <h1>All About Me!</h1>
+      <p>I like movies and blah blah blah blah blah</p>
+      <img src="#" />
+    </div>
+  );
+}
+
+export default ProfilePage;
---
+++ 02_Components/component_ref_component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Picture() {
+  return <img src="#" />;
+}
+
+function Profile() {
+  return (
+    <>
+      <Picture />
+      <h1>Name: Octavia</h1>
+      <h2>Species: Octopus</h2>
+       <h2>Class: Cephalopoda</h2>
+    </>
+  )
+}
+
+export default Profile;
