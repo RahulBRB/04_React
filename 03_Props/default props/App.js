94d72c6b59b78437965d7b7a2080a1c1ee1684df
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-08-03 14:24:56 +0530

    Create App.js

---
+++ 03_Props/default props/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from './Button.js';
+
+function App() {
+  return <Button text=''/>;  //passing empty string to child, so default one will be used
+}
+
+export default App
